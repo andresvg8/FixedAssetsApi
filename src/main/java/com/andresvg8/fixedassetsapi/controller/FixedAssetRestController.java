7bb935e9f08aef95d7d40142e4148963783fbd88
Andres Villalba Gaviria <66088651+andresvg8@users.noreply.github.com>
2022-04-05 16:15:21 -0500

    Updates and bug fixes for FixedAssetRestController.java to fix call methods from URL.

--- src/main/java/com/andresvg8/fixedassetsapi/controller/FixedAssetRestController.java
+++ src/main/java/com/andresvg8/fixedassetsapi/controller/FixedAssetRestController.java
@@ -9,6 +9,7 @@ import com.andresvg8.fixedassetsapi.service.CompanyAreaService;
 import com.andresvg8.fixedassetsapi.service.EmployeeService;
 import com.andresvg8.fixedassetsapi.service.FixedAssetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,13 +32,14 @@ public class FixedAssetRestController {
     @Autowired
     private FixedAssetService fixedAssetService;
 
-    @GetMapping(params = {"type"})
+    @GetMapping("/type/{type}")
+    @ResponseBody
     public ResponseEntity<?> getFixedAssetByType(
-            @RequestParam(value = "type") String type
+            @PathVariable String type
     ){
         Optional<List<FixedAsset>> optionalFixedAsset = fixedAssetService.findByType(type);
         if(!optionalFixedAsset.isPresent()){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error. Cannot recover any data from the database.");
         }
         if(optionalFixedAsset.get().size()<1){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("There are no data with the given asset type.");
@@ -45,13 +47,14 @@ public class FixedAssetRestController {
         return ResponseEntity.ok(optionalFixedAsset.get());
     }
 
-    @GetMapping(params = {"purchaseDate"})
+    @GetMapping("/purchase_date/{purchaseDate}")
+    @ResponseBody
     public ResponseEntity<?> getFixedAssetByPurchaseDate(
-            @RequestParam(value = "purchaseDate") LocalDate purchaseDate
+            @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate purchaseDate
     ){
         Optional<List<FixedAsset>> optionalFixedAsset = fixedAssetService.findByPurchaseDate(purchaseDate);
         if(!optionalFixedAsset.isPresent()){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error. Cannot retrieve data from the database.");
         }
         if(optionalFixedAsset.get().size()<1){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("There are no data with the given purchase date.");
@@ -59,9 +62,9 @@ public class FixedAssetRestController {
         return ResponseEntity.ok(optionalFixedAsset.get());
     }
 
-    @GetMapping(params = {"serial"})
+    @GetMapping("/serial/{serial}")
     public ResponseEntity<?> getFixedAssetBySerial(
-            @RequestParam(value = "serial") String serial
+            @PathVariable String serial
     ){
         Optional<List<FixedAsset>> optionalFixedAsset = fixedAssetService.findBySerial(serial);
         if(!optionalFixedAsset.isPresent()){
@@ -93,7 +96,7 @@ public class FixedAssetRestController {
             @NotNull @RequestBody FixedAsset fixedAsset
     ){
         if(fixedAsset.getId()==null){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The given asset sshould have an id number.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The given asset should have an id number.");
         }
         Optional<FixedAsset> optionalFixedAsset = fixedAssetService.findById( fixedAsset.getId() );
         if(!optionalFixedAsset.isPresent()){
@@ -103,79 +106,94 @@ public class FixedAssetRestController {
         return ResponseEntity.status(HttpStatus.OK).body(fixedAsset);
     }
 
-    @GetMapping("/setdefaults")
-    public ResponseEntity<?> setDefaultValues(){
-        try{
-            String[] cityNames = {"Bogotá", "Cali", "Villavicencio"};
-            for(String cityName : cityNames){
-                City city = new City();
-                city.setName(cityName);
-                city = cityService.save(city);
-            }
-            List<City> cities = cityService.findAll();
-            List<String> areaNames = Arrays.asList("Management", "IT");
-            for(City city : cities){
-                for(String areaName : areaNames){
-                    System.out.println("\t - - - - - - - - - - - - - - - - - ");
-                    CompanyArea companyArea = new CompanyArea();
-                    companyArea.setName(areaName);
-                    companyArea = companyAreaService.save(companyArea);
-                    companyArea.getCities().add(city);
-                    companyArea = companyAreaService.save(companyArea);
-                    System.out.println("\t" + companyArea.toString());
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllAssets(){
+        this.setFakeValuesIfPossible();
+        Optional<List<FixedAsset>> optionalAssets = fixedAssetService.findAll();
+        if(!optionalAssets.isPresent()){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error. Can not recover data from the database.");
+        }
+        if(optionalAssets.get().size()<1){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("There is no assets data stored in the database.");
+        }
+        return ResponseEntity.ok(optionalAssets.get());
+    }
+
+    private boolean areFakeValuesSetted = false;
+    //@GetMapping("/setdefaults")
+    private ResponseEntity<?> setFakeValuesIfPossible(){
+        if( !(this.areFakeValuesSetted) ){
+            try{
+                String[] cityNames = {"Bogotá", "Cali", "Villavicencio"};
+                for(String cityName : cityNames){
+                    City city = new City();
+                    city.setName(cityName);
+                    city = cityService.save(city);
+                }
+                List<City> cities = cityService.findAll();
+                List<String> areaNames = Arrays.asList("Management", "IT");
+                for(City city : cities){
+                    for(String areaName : areaNames){
+                        CompanyArea companyArea = new CompanyArea();
+                        companyArea.setName(areaName);
+                        companyArea = companyAreaService.save(companyArea);
+                        companyArea.getCities().add(city);
+                        companyArea = companyAreaService.save(companyArea);
+                    }
+                }
+                List<CompanyArea> companyAreas = companyAreaService.findAll();
+                System.out.println("Company areas: ");
+                for(CompanyArea area : companyAreas){
+                    System.out.println(""+area.toString());
+                }
+
+                String[] employeeNames = {"Andrés Villalba Gaviria", "Employee A", "Employee B", "Employee C", "Employee D", "Employee E", "Employee F", "Employee G", "Employee H", "Employee I"};
+                for(String employeeName : employeeNames){
+                    Employee employee = new Employee();
+                    employee.setName(employeeName);
+                    employee = employeeService.save(employee);
+                }
+                int assetsQuantity = 10;
+                String[] assetNames =               {"Asus TUF F15",        "Asus Vivobook",    "Lenovo Thinkpad",  "Workstation",          "Workstation",          "Workstation",          "Workstation",          "Blue bag",                 "Brown bag",                "Gray bag"};
+                String[] assetDescriptions =        {"Laptop computer",     "Laptop computer",  "Laptop computer",  "Desktop and chair",    "Desktop and chair",    "Desktop and chair",    "Desktop and chair",    "Bag to carry computer",    "Bag to carry computer",    "Bag to carry computer"};
+                String[] assetTypes =               {"Computer",            "Computer",         "Computer",         "Desktop",              "Desktop",              "Desktop",              "Desktop",              "Bag",                      "Bag",                      "Bag"};
+                String[] assetSerials =             {"12345",               "67890",            "11223",            "34455",                "66778",                "89900",                "11122",                "23334",                    "44555",                    "66677"};
+                String[] assetInventoryNumbers =    {"C01",                 "C02",              "C03",              "D01",                  "D02",                  "D03",                  "D04",                  "B01",                      "B02",                      "B03"};
+                Double[] assetWeights = new Double[assetsQuantity];
+                Double[] assetHeights = new Double[assetsQuantity];
+                Double[] assetWidths = new Double[assetsQuantity];
+                Double[] assetLongitudes = new Double[assetsQuantity];
+                Double[] assetPurchasePrices = new Double[assetsQuantity];
+                LocalDate[] assetPurchaseDates = new LocalDate[assetsQuantity];
+                for(int x=0; x<assetsQuantity; x++){
+                    assetWeights[x] = Math.random()*4;
+                    assetHeights[x] = Math.random()*6;
+                    assetWidths[x] = Math.random()*6;
+                    assetLongitudes[x] = Math.random()*7;
+                    assetPurchasePrices[x] = Math.random()*4000000;
+                    assetPurchaseDates[x] = LocalDate.of(2019+(int)(Math.random()*3), 1+(int)(Math.random()*12), 1+(int)(Math.random()*28));
+                }
+                for(int x=0; x<assetsQuantity; x++){
+                    FixedAsset asset = new FixedAsset();
+                    asset.setName(assetNames[x]);
+                    asset.setDescription(assetDescriptions[x]);
+                    asset.setType(assetTypes[x]);
+                    asset.setSerial(assetSerials[x]);
+                    asset.setInventoryNumber(assetInventoryNumbers[x]);
+                    asset.setWeight(assetWeights[x]);
+                    asset.setHeight(assetHeights[x]);
+                    asset.setWidth(assetWidths[x]);
+                    asset.setLongitude(assetLongitudes[x]);
+                    asset.setPurchasePrice(assetPurchasePrices[x]);
+                    asset.setPurchaseDate(assetPurchaseDates[x]);
+                    fixedAssetService.save(asset);
                 }
             }
-            List<CompanyArea> companyAreas = companyAreaService.findAll();
-            System.out.println("Company areas: ");
-            for(CompanyArea area : companyAreas){
-                System.out.println(""+area.toString());
-            }
-
-            String[] employeeNames = {"Andrés Villalba Gaviria", "Employee A", "Employee B", "Employee C", "Employee D", "Employee E", "Employee F", "Employee G", "Employee H", "Employee I"};
-            for(String employeeName : employeeNames){
-                Employee employee = new Employee();
-                employee.setName(employeeName);
-                employee = employeeService.save(employee);
-            }
-            int assetsQuantity = 10;
-            String[] assetNames =               {"Asus TUF F15",        "Asus Vivobook",    "Lenovo Thinkpad",  "Workstation",          "Workstation",          "Workstation",          "Workstation",          "Blue bag",                 "Brown bag",                "Gray bag"};
-            String[] assetDescriptions =        {"Laptop computer",     "Laptop computer",  "Laptop computer",  "Desktop and chair",    "Desktop and chair",    "Desktop and chair",    "Desktop and chair",    "Bag to carry computer",    "Bag to carry computer",    "Bag to carry computer"};
-            String[] assetTypes =               {"Computer",            "Computer",         "Computer",         "Desktop",              "Desktop",              "Desktop",              "Desktop",              "Bag",                      "Bag",                      "Bag"};
-            String[] assetSerials =             {"12345",               "67890",            "11223",            "34455",                "66778",                "89900",                "11122",                "23334",                    "44555",                    "66677"};
-            String[] assetInventoryNumbers =    {"C01",                 "C02",              "C03",              "D01",                  "D02",                  "D03",                  "D04",                  "B01",                      "B02",                      "B03"};
-            Double[] assetWeights = new Double[assetsQuantity];
-            Double[] assetHeights = new Double[assetsQuantity];
-            Double[] assetWidths = new Double[assetsQuantity];
-            Double[] assetLongitudes = new Double[assetsQuantity];
-            Double[] assetPurchasePrices = new Double[assetsQuantity];
-            LocalDate[] assetPurchaseDates = new LocalDate[assetsQuantity];
-            for(int x=0; x<assetsQuantity; x++){
-                assetWeights[x] = Math.random()*4;
-                assetHeights[x] = Math.random()*6;
-                assetWidths[x] = Math.random()*6;
-                assetLongitudes[x] = Math.random()*7;
-                assetPurchasePrices[x] = Math.random()*4000000;
-                assetPurchaseDates[x] = LocalDate.of(2019+(int)(Math.random()*3), 1+(int)(Math.random()*12), 1+(int)(Math.random()*28));
-            }
-            for(int x=0; x<assetsQuantity; x++){
-                FixedAsset asset = new FixedAsset();
-                asset.setName(assetNames[x]);
-                asset.setDescription(assetDescriptions[x]);
-                asset.setType(assetTypes[x]);
-                asset.setSerial(assetSerials[x]);
-                asset.setInventoryNumber(assetInventoryNumbers[x]);
-                asset.setWeight(assetWeights[x]);
-                asset.setHeight(assetHeights[x]);
-                asset.setWidth(assetWidths[x]);
-                asset.setLongitude(assetLongitudes[x]);
-                asset.setPurchasePrice(assetPurchasePrices[x]);
-                asset.setPurchaseDate(assetPurchaseDates[x]);
-                fixedAssetService.save(asset);
+            catch (Exception exception){
+                return ResponseEntity.internalServerError().body("Internal server error.");
             }
         }
-        catch (Exception exception){
-            return ResponseEntity.internalServerError().body("Internal server error.");
-        }
+        this.areFakeValuesSetted = true;
         return ResponseEntity.ok("Default values were setted into the database.");
     }
 }
